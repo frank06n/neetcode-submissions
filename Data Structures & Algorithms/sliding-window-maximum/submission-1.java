8b19fd02475d8279b441844782ef74554fc75a93
Pritam Das <dev.code38@gmail.com>
2026-09-13 06:17:50 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.java
@@ -0,0 +1,50 @@
+class Solution {
+    class Pair implements Comparable<Pair> {
+        int val;
+        int ix;
+        Pair(int val, int ix) {
+            this.val = val;
+            this.ix = ix;
+        }
+
+        @Override
+        public int compareTo(Pair p) {
+            if (p == null)
+            throw new NullPointerException("Cannot compare with null");
+            
+            return Integer.compare(this.val, p.val);
+        }
+    }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int z = k-1;
+        int[] out = new int[nums.length-z];
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            Collections.reverseOrder()
+        );
+
+        for (int i=0; i<nums.length; i++)
+        {
+            pq.offer(new Pair(nums[i], i));
+            if (i >= z) {
+                Pair mp = pq.peek();
+                while (mp.ix < i-z) {
+                    pq.poll();
+                    mp = pq.peek();
+                }
+                out[i-z] = mp.val;
+            }
+        }
+
+        // for (int i=z; i<nums.length; i++)
+        // {
+        //     int max = nums[i];
+        //     for (int j=i-1; j>=i-z; j--)
+        //         if (nums[j]>max) max = nums[j];
+
+        //     out[i-z] = max;
+        // }
+        return out;
+    }
+}
