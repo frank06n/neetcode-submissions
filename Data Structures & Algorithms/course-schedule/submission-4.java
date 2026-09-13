dcd29e78c8f6b3e70671d0b6c5d0e5807ed3dca1
Pritam Das <dev.code38@gmail.com>
2026-09-13 07:23:27 +0530

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.java
@@ -0,0 +1,48 @@
+class Solution {
+    List<Integer>[] graph;
+    int[] state;
+
+    public boolean canFinish(int nc, int[][] pre) {
+        graph = new List[nc];
+        state = new int[nc];
+
+        for (int[] preq : pre)
+        {
+            List<Integer> edges = graph[preq[1]];
+            if (edges == null)
+            {
+                edges = graph[preq[1]] = new ArrayList();
+            }
+            edges.add(preq[0]);
+        }
+
+        for (int i=0; i<nc; i++)
+        {
+            if (state[i] == 2) continue;
+            boolean hasCycle = visit(i);
+
+            if (hasCycle) return false;
+        }
+
+        return true;
+    }
+
+    // return TRUE if some node is already traversed
+    boolean visit(int v) {
+        if (state[v] == 1) return true;
+        if (state[v] == 2) return false;
+        
+        state[v] = 1;
+
+        if (graph[v] != null) {
+            for (int childV : graph[v]) 
+            {
+                boolean hasCycle = visit(childV); 
+                if (hasCycle) return true;
+            }
+        }
+
+        state[v] = 2;
+        return false;
+    }
+}
