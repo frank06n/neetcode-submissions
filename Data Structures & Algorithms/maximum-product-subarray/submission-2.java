316ed79b10cf93d7f13d36cbf4329cbd80657e94
Pritam Das <dev.code38@gmail.com>
2026-09-21 13:37:41 +0530

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        int allmax = nums[0]; // max of all
+        
+        int fwn = 0; // first with negative
+        int max = 0; // max of curr
+        int mxwn = 0; // max with negative
+        int encn = 0; // true/false - encountered negative
+
+        for (int i=0; i<nums.length; i++)
+        {
+            if (nums[i] == 0)
+            {
+                fwn=max=mxwn=encn=0;
+                if (allmax < 0) allmax = 0;
+            }
+            else if (nums[i] < 0)
+            {
+                mxwn = (mxwn == 0) ? nums[i] : (mxwn*nums[i]);
+                max = (mxwn > 0) ? mxwn : 0;
+
+                if (mxwn > allmax) allmax = mxwn;
+                if (mxwn < 0 && encn==1 && mxwn/fwn > allmax) allmax = mxwn/fwn;
+
+                if (encn==0)
+                {
+                    encn=1;
+                    fwn = mxwn;
+                }
+            }
+            else
+            {
+                mxwn = (mxwn == 0) ? nums[i] : (mxwn*nums[i]);
+                max = (max == 0) ? nums[i] : (max*nums[i]);
+
+                if (max > allmax) allmax = max;
+                if (mxwn < 0 && encn==1 && mxwn/fwn > allmax) allmax = mxwn/fwn;
+            }
+            // System.out.printf("n: %d, max: %d, mxwn: %d, fwn: %d, allmax: %d\n", nums[i], max, mxwn, fwn, allmax);
+        }
+
+        return allmax;
+    }
+}
